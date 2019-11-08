d155578d5c96cc2aeddc03bd86030f49606d9bf3
Ben Tagtow <bentagtow@gmail.com>
2019-11-08 19:55:07 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,13 +6,37 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
+  array_1 = [['pepperoni', 'sausage'], ['green olives', 'green peppers'], ['onions', 'pineapple']]
+  row_index= 0 
+  while row_index<array_1.count do 
+    element_index=0 
+    while element_index<array_1[row_index].count do
+     result = 
+       "I love #{array_1[row_index][element_index]} and #{array_1[row_index][element_index+1]} on my pizza",
+       "I love #{array_1[row_index+1][element_index]} and #{array_1[row_index+1][element_index+1]} on my pizza",
+       "I love #{array_1[row_index+2][element_index]} and #{array_1[row_index+2][element_index+1]} on my pizza"
+     return result
+      element_index += 1 
+    end 
+    row_index+=1 
+  end 
 end
 
 def find_greater_pair(src)
+  new_array=[]
+  row_index=0 
+  while row_index<src.count do 
+    if src[row_index].first>src[row_index].last
+      new_array.unshift(src[row_index].first)
+    else new_array.unshift([row_index].last)
+    end 
+  end
+  row_index+=1
+end
+
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
-end
 
 def total_even_pairs(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
